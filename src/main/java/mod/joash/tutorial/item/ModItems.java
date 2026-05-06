7a0e1c3af822d5150ef048dc0412476dda0ace5b
joashneves <joashneves@gmail.com>
2026-05-06 12:09:34 -0300

    first commit

---
+++ src/main/java/mod/joash/tutorial/item/ModItems.java
@@ -0,0 +1,28 @@
+package mod.joash.tutorial.item;
+
+import mod.joash.tutorial.Tutorialmod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Tutorialmod.MOD_ID, name), item);
+    }
+
+    public static void registerItems() {
+        Tutorialmod.LOGGER.info("Registering Items : " + Tutorialmod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
