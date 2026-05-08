8239607199026569b8ecf462e90ab339278cdba1
joashneves <joashneves@gmail.com>
2026-05-08 12:01:09 -0300

    criar custom tags

--- src/main/java/mod/joash/tutorial/block/custom/MagicBlock.java
+++ src/main/java/mod/joash/tutorial/block/custom/MagicBlock.java
@@ -1,6 +1,6 @@
 package mod.joash.tutorial.block.custom;
 
-import mod.joash.tutorial.item.ModItems;
+import mod.joash.tutorial.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -35,12 +35,16 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
